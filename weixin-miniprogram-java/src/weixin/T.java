235f6019841c35719c19126fd60a9c3d6697ca0e
lihuadong <lihuadongv@163.com>
2020-01-15 10:47:55 +0800

    初始化

---
+++ weixin-miniprogram-java/src/weixin/T.java
@@ -0,0 +1,12 @@
+package weixin;
+
+public class T {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+        System.out.println("hi,fullstackeer");
+        
+	}
+
+}
